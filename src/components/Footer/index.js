0683cc6276913de14d1c89d7c08a72fb56b68568
Calin Puscas <calinpuscas@gmail.com>
2021-06-14 18:40:22 +0300

    third commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const Footer = (props) => {
+  return (
+    <footer className="footer">
+      <div className="wrap">© 2021 - ElysianBijoux™. All rights reserved.</div>
+    </footer>
+  );
+};
+
+export default Footer;
